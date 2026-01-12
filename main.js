39ad318f90dc116bcb82f6cc579b88b6763e6ad9
giovannimazzi <gioelor@libero.it>
2026-01-12 14:43:12 +0100

    inserita selezione elementi del DOM da manipolare

---
+++ main.js
@@ -0,0 +1,6 @@
+// seleziono gli elementi del DOM che devo manipolare
+const lampImage = document.getElementById("lamp-img");
+const lampButton = document.getElementById("lamp-button");
+
+console.log(lampImage.src);
+console.log(lampButton.textContent);
