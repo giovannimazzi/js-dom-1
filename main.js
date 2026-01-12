415889122fd8e55edd31c3e2620f9ea79e7f20de
giovannimazzi <gioelor@libero.it>
2026-01-12 14:47:55 +0100

    inserito commento chiarificatore

--- main.js
+++ main.js
@@ -2,6 +2,7 @@
 const lampImage = document.getElementById("lamp-img");
 const lampButton = document.getElementById("lamp-button");
 
+// funzione che si attiva al click sul pulsante
 lampButton.addEventListener("click", function () {
   console.log(lampImage.src);
   console.log(lampButton.textContent);
