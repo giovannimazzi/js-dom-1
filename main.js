69864968bd06b2558650d51dd5b988b830ace532
giovannimazzi <gioelor@libero.it>
2026-01-12 15:47:11 +0100

    inserita versione con utilizzo di string.includes()

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const lightONPath = "./img/yellow_lamp.png";
 const lightOFFPath = "./img/white_lamp.png";
 const turnONCMD = "ACCENDI";
 const turnOFFCMD = "SPEGNI";
-let lampState;
+/* let lampState; */
 let consoleColor;
 
 // selezione elementi del DOM da manipolare
@@ -12,25 +12,30 @@ const lampButton = document.getElementById("lamp-button");
 
 // al caricamento della pagina
 lampImage.src = lightOFFPath; // luce OFF
-lampState = "OFF";
+/* lampState = "OFF"; */
 lampButton.textContent = turnONCMD; // segnale di accensione luce
 
 // funzione che si attiva al click sul pulsante
 lampButton.addEventListener("click", function () {
-  if (lampState === "OFF") {
+  /* if (lampState === "OFF") { */
+  if (lampImage.src.includes("white")) {
     lampImage.src = lightONPath;
     lampButton.textContent = turnOFFCMD;
     lampButton.classList.remove("btn-warning");
     lampButton.classList.add("btn-primary");
-    lampState = "ON";
+    /* lampState = "ON"; */
     consoleColor = "color: yellow";
   } else {
     lampImage.src = lightOFFPath;
     lampButton.textContent = turnONCMD;
     lampButton.classList.remove("btn-primary");
     lampButton.classList.add("btn-warning");
-    lampState = "OFF";
+    /* lampState = "OFF"; */
     consoleColor = "color: cyan";
   }
-  console.log(`STATO LAMPADA: %c${lampState}`, consoleColor);
+  /* console.log(`STATO LAMPADA: %c${lampState}`, consoleColor); */
+  console.log(
+    `STATO LAMPADA: %c${lampImage.src.includes("white") ? "OFF" : "ON"}`,
+    consoleColor
+  );
 });
