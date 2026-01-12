4c3d0afd9ec0f54622879243275556a59aafeb75
giovannimazzi <gioelor@libero.it>
2026-01-12 16:35:46 +0100

    inserita versione con doppia immagine caricata e gestione ON-OFF tramite display

--- main.js
+++ main.js
@@ -7,28 +7,39 @@ const turnOFFCMD = "SPEGNI";
 let consoleColor;
 
 // selezione elementi del DOM da manipolare
-const lampImage = document.getElementById("lamp-img");
+const lampOFFImage = document.getElementById("lampOFF-img");
+const lampONImage = document.getElementById("lampON-img");
 const lampButton = document.getElementById("lamp-button");
 
 // al caricamento della pagina
-lampImage.src = lightOFFPath; // luce OFF
+lampOFFImage.src = lightOFFPath; // luce OFF
+lampONImage.src = lightONPath; // luce ON
 /* lampState = "OFF"; */
 lampButton.textContent = turnONCMD; // segnale di accensione luce
 
 // funzione che si attiva al click sul pulsante
 lampButton.addEventListener("click", function () {
   /* if (lampState === "OFF") { */
-  if (lampImage.src.includes("white")) {
-    lampImage.src = lightONPath;
-    lampImage.classList.add("lampON");
+  /* if (lampImage.src.includes("white")) { */
+  if (lampONImage.classList.contains("d-none")) {
+    /* lampImage.src = lightONPath; */
+    /* lampImage.classList.add("lampON"); */
+    lampOFFImage.classList.add("d-none");
+    lampOFFImage.classList.remove("d-block");
+    lampONImage.classList.remove("d-none");
+    lampONImage.classList.add("d-block");
     lampButton.textContent = turnOFFCMD;
     lampButton.classList.remove("btn-warning");
     lampButton.classList.add("btn-primary");
     /* lampState = "ON"; */
     consoleColor = "color: yellow";
   } else {
-    lampImage.src = lightOFFPath;
-    lampImage.classList.remove("lampON");
+    /* lampImage.src = lightOFFPath; */
+    /* lampImage.classList.remove("lampON"); */
+    lampONImage.classList.add("d-none");
+    lampONImage.classList.remove("d-block");
+    lampOFFImage.classList.remove("d-none");
+    lampOFFImage.classList.add("d-block");
     lampButton.textContent = turnONCMD;
     lampButton.classList.remove("btn-primary");
     lampButton.classList.add("btn-warning");
@@ -36,8 +47,14 @@ lampButton.addEventListener("click", function () {
     consoleColor = "color: cyan";
   }
   /* console.log(`STATO LAMPADA: %c${lampState}`, consoleColor); */
-  console.log(
+  /* console.log(
     `STATO LAMPADA: %c${lampImage.src.includes("white") ? "OFF" : "ON"}`,
+    consoleColor
+  ); */
+  console.log(
+    `STATO LAMPADA: %c${
+      lampONImage.classList.contains("d-none") ? "OFF" : "ON"
+    }`,
     consoleColor
   );
 });
