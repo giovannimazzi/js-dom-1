ebbaaa69382ce3acdf26aa2b3d513acd2e780b50
giovannimazzi <gioelor@libero.it>
2026-01-12 15:19:46 +0100

    inserita funzione che accende la lampadina alla pressione del pulsante, ed alcune utilities.

--- main.js
+++ main.js
@@ -1,9 +1,27 @@
+// utilities
+const lightONPath = "./img/yellow_lamp.png";
+const lightOFFPath = "./img/white_lamp.png";
+const turnONCMD = "ACCENDI";
+const turnOFFCMD = "SPEGNI";
+let lampState;
+
 // selezione elementi del DOM da manipolare
 const lampImage = document.getElementById("lamp-img");
 const lampButton = document.getElementById("lamp-button");
 
+// al caricamento della pagina
+lampImage.src = lightOFFPath; // luce OFF
+lampState = "OFF";
+lampButton.textContent = turnONCMD; // segnale di accensione luce
+
 // funzione che si attiva al click sul pulsante
 lampButton.addEventListener("click", function () {
-  console.log(lampImage.src);
-  console.log(lampButton.textContent);
+  if (lampState === "OFF") {
+    lampImage.src = lightONPath;
+    lampButton.textContent = turnOFFCMD;
+    lampButton.classList.remove("btn-warning");
+    lampButton.classList.add("btn-primary");
+    lampState = "ON";
+    console.log(`STATO LAMPADA: ${lampState}`);
+  }
 });
