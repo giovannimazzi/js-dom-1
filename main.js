518b03e1f89f3baa2fcbd5bbd48f791b1c7215c8
giovannimazzi <gioelor@libero.it>
2026-01-12 18:00:34 +0100

    ottimizzato togliendo un annidamento

--- main.js
+++ main.js
@@ -24,51 +24,52 @@ lampButton.textContent = turnONCMD; // segnale di accensione luce
 
 // funzione che si attiva al click sul pulsante
 lampButton.addEventListener("click", function () {
-  if (!(lampState === "BROKEN")) {
-    if (onOffCount < brokenThreshold - 1) {
-      if (lampState === "OFF") {
-        /* if (lampImage.src.includes("white")) { */
-        /* if (lampONImage.classList.contains("d-none")) { */
-        /* lampImage.src = lightONPath; */
-        /* lampImage.classList.add("lampON"); */
-        lampOFFImage.classList.add("d-none");
-        lampOFFImage.classList.remove("d-block");
-        lampONImage.classList.remove("d-none");
-        lampONImage.classList.add("d-block");
-        lampButton.textContent = turnOFFCMD;
-        lampButton.classList.remove("btn-warning");
-        lampButton.classList.add("btn-primary");
-        lampState = "ON";
-        consoleColor = "color: yellow";
-      } else {
-        /* lampImage.src = lightOFFPath; */
-        /* lampImage.classList.remove("lampON"); */
-        lampONImage.classList.add("d-none");
-        lampONImage.classList.remove("d-block");
-        lampOFFImage.classList.remove("d-none");
-        lampOFFImage.classList.add("d-block");
-        lampButton.textContent = turnONCMD;
-        lampButton.classList.remove("btn-primary");
-        lampButton.classList.add("btn-warning");
-        lampState = "OFF";
-        consoleColor = "color: cyan";
-      }
-    } else {
+  if (lampState === "BROKEN") {
+    return;
+  }
+  if (onOffCount < brokenThreshold - 1) {
+    if (lampState === "OFF") {
+      /* if (lampImage.src.includes("white")) { */
+      /* if (lampONImage.classList.contains("d-none")) { */
+      /* lampImage.src = lightONPath; */
+      /* lampImage.classList.add("lampON"); */
       lampOFFImage.classList.add("d-none");
       lampOFFImage.classList.remove("d-block");
+      lampONImage.classList.remove("d-none");
+      lampONImage.classList.add("d-block");
+      lampButton.textContent = turnOFFCMD;
+      lampButton.classList.remove("btn-warning");
+      lampButton.classList.add("btn-primary");
+      lampState = "ON";
+      consoleColor = "color: yellow";
+    } else {
+      /* lampImage.src = lightOFFPath; */
+      /* lampImage.classList.remove("lampON"); */
       lampONImage.classList.add("d-none");
       lampONImage.classList.remove("d-block");
-      lampBROKENImage.classList.add("d-block");
-      lampBROKENImage.classList.remove("d-none");
-      lampButton.textContent = "ROTTA!";
-      lampButton.classList.remove("btn-warning");
-      lampButton.classList.add("btn-danger");
-      lampButton.disabled = true;
-      lampState = "BROKEN";
-      consoleColor = "color: orangered";
+      lampOFFImage.classList.remove("d-none");
+      lampOFFImage.classList.add("d-block");
+      lampButton.textContent = turnONCMD;
+      lampButton.classList.remove("btn-primary");
+      lampButton.classList.add("btn-warning");
+      lampState = "OFF";
+      consoleColor = "color: cyan";
     }
-    onOffCount++;
+  } else {
+    lampOFFImage.classList.add("d-none");
+    lampOFFImage.classList.remove("d-block");
+    lampONImage.classList.add("d-none");
+    lampONImage.classList.remove("d-block");
+    lampBROKENImage.classList.add("d-block");
+    lampBROKENImage.classList.remove("d-none");
+    lampButton.textContent = "ROTTA!";
+    lampButton.classList.remove("btn-warning");
+    lampButton.classList.add("btn-danger");
+    lampButton.disabled = true;
+    lampState = "BROKEN";
+    consoleColor = "color: orangered";
   }
+  onOffCount++;
 
   console.log(
     `STATO LAMPADA: %c${lampState}%c [${onOffCount}]`,
